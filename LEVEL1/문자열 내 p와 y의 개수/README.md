c5f202edbbeaabee23d6d3576e7170792964984a
77JY <kwn03111@naver.com>
2023-09-11 10:05:02 +0900

    문자열 내 p와 y의 개수

---
+++ LEVEL1/문자열 내 p와 y의 개수/README.md
@@ -0,0 +1,3 @@
+P 와 Y수가 같으면 True 다르면 False
+문자열을 lower로 소문자로 만든뒤
+count함수로 p의 수와 y수가 같으면 true 리턴 아니면 false
---
+++ LEVEL1/문자열 내 p와 y의 개수/python.py
@@ -0,0 +1,6 @@
+def solution(s):
+    answer = False
+    s = s.lower()
+    if s.count('p') == s.count('y'):
+        return True
+    return answer
