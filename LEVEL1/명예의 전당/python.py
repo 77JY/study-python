b1a8cbeb049310d21a127f52f5c4a9ef7b0546de
77JY <kwn03111@naver.com>
2023-09-24 23:56:04 +0900

    명예의 전당 (1) 수정
    https://school.programmers.co.kr/learn/courses/30/lessons/138477
    
    쓸데없이 한번더 변수선언한것 취소

--- LEVEL1/명예의 전당/python.py
+++ LEVEL1/명예의 전당/python.py
@@ -3,8 +3,7 @@ def solution(k, score):
     answer = []
     for i in score:
         s_list.append(i)
-        min_num = min(s_list)
         if k < len(s_list):
-            s_list.remove(min_num)
+            s_list.remove(min(s_list))
         answer.append(min(s_list))
     return(answer)
