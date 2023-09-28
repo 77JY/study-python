485954722202b032c5ef31eab9bc666a8b77d289
77JY <kwn03111@naver.com>
2023-09-28 23:25:48 +0900

    달리기 경주
    https://school.programmers.co.kr/learn/courses/30/lessons/178871

---
+++ LEVEL1/달리기 경주/python.py
@@ -0,0 +1,11 @@
+def solution(players, callings):
+    dic = {players[i] : i for i in range(len(players))}
+    
+    for i in callings:
+        idx = dic[i]
+        dic[i] -= 1
+        dic[players[idx - 1]] += 1
+        players[idx] = players[idx - 1]
+        players[idx - 1] = i
+
+    return players
